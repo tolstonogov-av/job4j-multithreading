ce8f4feba1964a3800662654dc5f046e1bebcc3c
tolstinator <tolstonogov@mail.ru>
2020-10-23 11:48:09 +0700

    3. Прерывание нити [#314461].

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,36 @@
+package ru.job4j.concurrent;
+
+/**
+ * Simulating loading process for a specified time.
+ *
+ * @author Tolstonogov Alexey
+ * @version 1.0
+ */
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        char[] process = {'\\', '|', '/'};
+        int i = 0;
+        boolean increase = true;
+        System.out.print("\rLoading..." + process[i++]);
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                break;
+            }
+            if (i == 2 || i == 0) {
+                increase = !increase;
+            }
+            System.out.print("\rLoading..." + process[increase ? i++ : i--]);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(2000);
+        progress.interrupt();
+    }
+}
