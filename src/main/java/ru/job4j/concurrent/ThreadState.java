b0178ed02e2f5807c2783e041229cebf2c7653bf
tolstinator <tolstonogov@mail.ru>
2020-10-23 10:49:05 +0700

    1.1. Состояние нити. [#314457].

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,24 @@
+package ru.job4j.concurrent;
+
+/**
+ * Display names of threads and message, when threads are finished.
+ *
+ * @author Tolstonogov Alexey
+ * @version 1.0
+ */
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED
+                || second.getState() != Thread.State.TERMINATED) {
+        }
+        System.out.println(first.getName() + " and " + second.getName() + " are finished.");
+    }
+}
