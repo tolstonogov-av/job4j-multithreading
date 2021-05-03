9f859b56e2cd7401ce84d22a24cbb4e4c349f370
tolstinator <tolstonogov@mail.ru>
2021-05-04 06:58:21 +0700

    WIP.

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -6,8 +6,18 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class ThreadPool {
+
+    private static final int TASKS_COUNT = 3;
+
     private final List<Thread> threads = new LinkedList<>();
-    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>();
+
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(TASKS_COUNT);
+
+    public ThreadPool() {
+        for (int i = 0; i <= Runtime.getRuntime().availableProcessors(); i++) {
+            threads.add(new Thread());
+        }
+    }
 
     public void work(Runnable job) {
 
