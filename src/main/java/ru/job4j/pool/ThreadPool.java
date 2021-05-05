cbbb0b90d619fb6e545ea51dc13ff15eb84cab52
tolstinator <tolstonogov@mail.ru>
2021-05-05 07:01:02 +0700

    WIP.

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -13,17 +13,37 @@ public class ThreadPool {
 
     private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(TASKS_COUNT);
 
+    private boolean softShutdown = false;
+
     public ThreadPool() {
-        for (int i = 0; i <= Runtime.getRuntime().availableProcessors(); i++) {
-            threads.add(new Thread());
+        initThreads(Runtime.getRuntime().availableProcessors());
+    }
+
+    private void initThreads (int size) {
+        for (int i = 0; i <= size; i++) {
+            threads.add(new Thread(() -> {
+                while (!softShutdown) {
+                    try {
+                        Runnable task = tasks.poll();
+                        task.run();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }));
         }
     }
 
     public void work(Runnable job) {
-
+        tasks.offer(job);
     }
 
     public void shutdown() {
+        threads.forEach(Thread::interrupt);
+    }
 
+    public void softShutdown() {
+        this.softShutdown = true;
     }
 }
